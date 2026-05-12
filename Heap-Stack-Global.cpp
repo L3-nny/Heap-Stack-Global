3a46b9b7e1051a2530600233f25dbc2cbad3170b
L3-nny <mubealenny@gmail.com>
2026-05-12 21:25:43 +0300

    Allocate data in the Global memory

--- Heap-Stack-Global.cpp
+++ Heap-Stack-Global.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 class Home {
-    public:
-        string address;
-        bool hasGarage;
-    
     private:
         string address;
         int numRooms;
 
+    public:
+        double price;
+        bool hasGarage;
+    
         Home(string addr, int rooms, double p, bool g)
             : address(addr), numRooms(rooms), price(p), hasGarage(g) {}
 
@@ -19,6 +19,10 @@ class Home {
 };
 
 
-
 // GLOBAL MEMORY where the object exists for the entire program duration
 Home globalHome("Locle Street", 5, 250000.0, false);
+
+void displayHomeInfo(const Home& h) {
+    cout << "Home Address: " << h.address << endl; 
+    cout << "Price: $" << h.price << endl;
+}

4c1e21b734c20757d7fb69f51a755ae3a68428c5
L3-nny <mubealenny@gmail.com>
2026-05-13 22:01:36 +0300

    First template for inheritance exercise using Vehicles and Recreational Vehicles

---
+++ Vehicle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class Vehicle {
+    private:
+        string plateNumber;
+    
+    public:
+        string make;
+        double engineRating;
+
+};
+
+class Dwelling {
+    private:
+        string address;
+    
+    public:
+        int numRooms;
+        double price;
+        int squareFootage;
+
+};
